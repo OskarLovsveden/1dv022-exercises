2f1b96798af0fcdd7626d1259a4b382907e6e645
OskarLovsveden <ol222hf@student.lnu.se>
2019-11-27 15:04:13 +0100

    finishing up bartboard

--- bartboard/src/js/app.js
+++ bartboard/src/js/app.js
@@ -1,4 +1,9 @@
 import './bart-board.js'
 
-const bb1 = document.createElement('bart-board')
-document.querySelector('#board').appendChild(bb1)
+// const bb1 = document.createElement('bart-board')
+// bb1.setAttribute('text', 'This is working really well')
+// bb1.setAttribute('speed', 2)
+// bb1.addEventListener('filled', () => {
+//   bb1.wipeBoard()
+// })
+// document.querySelector('#board').appendChild(bb1)
--- bartboard/src/js/bart-board.js
+++ bartboard/src/js/bart-board.js
@@ -1,5 +1,27 @@
 const template = document.createElement('template')
 template.innerHTML = `
+<style>
+  :host {
+    background:#002418;
+    font-size: 1.2em;
+    color:white;
+    width:500px;
+    height:200px;
+    padding:10px;
+    border:6px solid #9b3b00;
+    border-bottom:12px solid #9b3b00;
+    overflow:hidden;
+    margin:10px;
+    float:left;
+    border-radius: 3px;
+  }
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
 <p id="text"></p>
 
 `
@@ -10,11 +32,65 @@ export class BartBoard extends window.HTMLElement {
 
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._p = this.shadowRoot.querySelector('#text')
+    this._intervalID = null
+    this._letter = 0
+    this._text = 'Låt stå!'
+    this._speed = 50
+  }
+
+  static get observedAttributes () {
+    return ['text', 'speed']
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (name === 'text') {
+      this._text = newValue
+    }
+    if (name === 'speed') {
+      this._speed = newValue
+    }
   }
 
   connectedCallback () {
-    this.classList.add('blackboard')
-    this.innerText = 'I love JS'
+    this.addEventListener('mousedown', this._onWrite)
+    this.addEventListener('mouseup', this.stopWrite)
+    this.addEventListener('mouseleave', this.stopWrite)
+  }
+
+  disconnectedCallback () {
+    this.removeEventListener('mousedown', this._onWrite)
+    this.removeEventListener('mouseup', this.stopWrite)
+    this.removeEventListener('mouseleave', this.stopWrite)
+    this.stopWrite()
+  }
+
+  _onWrite (event) {
+    this._intervalID = setInterval(() => {
+      if (this._p.offsetHeight >= this.offsetHeight) {
+        this.dispatchEvent(new window.CustomEvent('filled'))
+        this.stopWrite()
+        this.wipeBoard()
+        return
+      }
+
+      if (this._text) {
+        this._p.textContent += this._text.charAt(this._letter++)
+        if (this._letter >= this._text.length) {
+          this._p.textContent += ' '
+          this._letter = 0
+        }
+      }
+    }, this._speed)
+  }
+
+  stopWrite () {
+    clearTimeout(this._intervalID)
+  }
+
+  wipeBoard () {
+    this._p.textContent = ''
+    this._letter = 0
   }
 }
 
