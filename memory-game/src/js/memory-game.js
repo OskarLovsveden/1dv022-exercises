341dd6e8382d2e7927aee794a44e32de9622d31c
OskarLovsveden <ol222hf@student.lnu.se>
2019-12-16 20:10:12 +0100

    added a html-template

--- memory-game/src/js/memory-game.js
+++ memory-game/src/js/memory-game.js
@@ -1,3 +1,14 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+  #tileDiv img {
+    width: 15%;
+  }
+</style>
+<div id="tileDiv">
+</div>
+`
+
 /**
  *  A Class representing a game of Memory
  *
@@ -6,25 +17,33 @@
  * @extends {window.HTMLElement}
  */
 export class MemoryGame extends window.HTMLElement {
+  /**
+   *Creates an instance of MemoryGame.
+   * @param {number} [rows=4]
+   * @param {number} [cols=4]
+   * @memberof MemoryGame
+   */
   constructor (rows = 4, cols = 4) {
     super()
 
     this.attachShadow({
       mode: 'open'
     })
-    const div = document.createElement('div')
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this._tileDiv = this.shadowRoot.querySelector('#tileDiv')
 
     for (let i = 0; i < rows * cols; i++) {
       const img = document.createElement('img')
       img.setAttribute('src', 'image/0.png')
-      div.appendChild(img)
+      this._tileDiv.appendChild(img)
 
       if ((i + 1) % cols === 0) {
-        div.appendChild(document.createElement('br'))
+        this._tileDiv.appendChild(document.createElement('br'))
       }
     }
 
-    this.shadowRoot.appendChild(div)
+    this.shadowRoot.appendChild(this._tileDiv)
 
     this.rows = rows
     this.cols = cols
