7d5e511f9e05e972d694b8205370bb720d51cea3
OskarLovsveden <ol222hf@student.lnu.se>
2019-12-18 19:13:46 +0100

    finished code-a-long

--- memory-game/src/js/app.js
+++ memory-game/src/js/app.js
@@ -1,1 +1,5 @@
 import './memory-game.js'
+
+const memoryGame = document.createElement('memory-game')
+const body = document.querySelector('body')
+body.appendChild(memoryGame)
--- memory-game/src/js/memory-game.js
+++ memory-game/src/js/memory-game.js
@@ -1,16 +1,31 @@
-const mainTemplate = document.createElement('template')
-mainTemplate.innerHTML = `
+// Template for the "game-window" within memory-game.
+const gameTemplate = document.createElement('template')
+gameTemplate.innerHTML = `
 <style>
-  #tileDiv img {
-    width: 15%;
+  #gamediv img {
+    max-width: 100%;
+  }
+  a {
+    display: inline-block;
+    border-radius: 3%;
+  }
+  a:focus {
+    background-color: pink;
+    outline: none;
   }
 </style>
-<div id="tileDiv">
+<div>
+  <h3 id="tries">
+    Number of tries: 0
+  </h3>
+</div>
+<div id="gamediv">
 </div>
 `
 
-const imgTemplate = document.createElement('template')
-imgTemplate.innerHTML = `
+// Template for the tiles.
+const tileTemplate = document.createElement('template')
+tileTemplate.innerHTML = `
 <a href="#"><img src="image/0.png" alt="A memory tile" /></a>
 `
 
@@ -35,139 +50,177 @@ export class MemoryGame extends window.HTMLElement {
       mode: 'open'
     })
 
-    this.shadowRoot.appendChild(mainTemplate.content.cloneNode(true))
+    this.shadowRoot.appendChild(gameTemplate.content.cloneNode(true))
 
     this.rows = rows
     this.cols = cols
     this._tiles = this._getPictureArray()
-    this._tileDiv = this.shadowRoot.querySelector('#tileDiv')
+    this._gamediv = this.shadowRoot.querySelector('#gamediv')
     this._turn1 = null
     this._turn2 = null
     this._lastTile = null
+    this._currentImg = null
+    this._tries = 0
+    this._pairs = 0
+    this._index = 0
 
     this._tiles.forEach((tile, index) => {
-      const img = imgTemplate.content.cloneNode(true)
-      this._tileDiv.appendChild(img)
+      const img = tileTemplate.content.cloneNode(true)
+      this._gamediv.appendChild(img)
 
-      const listOfImg = this._tileDiv.getElementsByTagName('img')
-      const currentImg = listOfImg[index]
+      const listOfImg = this._gamediv.getElementsByTagName('img')
+      this._currentImg = listOfImg[index]
 
-      currentImg.addEventListener('click', event => {
-        console.log(event.target)
-        this._turnTile(tile, index, event.target)
-      })
-
-      currentImg.parentNode.addEventListener('keydown', (event) => {
-        if (event.defaultPrevented) {
-          return // Do nothing if event already handled
-        }
-
-        switch (event.code) {
-          case 'KeyS':
-          case 'ArrowDown':
-            // HANDLE DOWN
-            this._arrowDown(index)
-            break
-          case 'KeyW':
-          case 'ArrowUp':
-            // HANDLE UP
-            this._arrowUp(index)
-            break
-          case 'KeyA':
-          case 'ArrowLeft':
-            // HANDLE LEFT
-            this._arrowLeft(index)
-            break
-          case 'KeyD':
-          case 'ArrowRight':
-            // HANDLE RIGHT
-            this._arrowRight(index)
-            break
-          case 'Enter' :
-            // HANDLE ENTER
-            this._turnTile(tile, index, event.target)
-            console.log(event.target)
-            break
-        }
-        // Consume the event so it doesn't get handled twice
-        event.preventDefault()
-      }, true)
+      this._currentImg.setAttribute('data-tilenumber', index)
 
       if ((index + 1) % this.cols === 0) {
-        this._tileDiv.appendChild(document.createElement('br'))
+        this._gamediv.appendChild(document.createElement('br'))
       }
     })
 
-    this._tileLimit = this._tileDiv.getElementsByTagName('a').length - 1
+    this._tileLimit = this._gamediv.getElementsByTagName('a').length - 1
   }
 
-  connectedCallback () {
-    this._tileDiv.getElementsByTagName('a')[0].focus()
-  }
-
+  /**
+   * Method for when arrow down, or S is pressed on the keyboard.
+   *
+   * @param {number} index of the current tile in focus.
+   * @memberof MemoryGame
+   */
   _arrowDown (index) {
     const nextTile = index + this.cols
-
     if (nextTile <= this._tileLimit) {
-      this._tileDiv.getElementsByTagName('a')[nextTile].focus()
+      this._gamediv.getElementsByTagName('a')[nextTile].focus()
+    } else {
+      return
     }
+    this._index = nextTile
   }
 
+  /**
+   * Method for when arrow up, or W is pressed on the keyboard.
+   *
+   * @param {number} index of the current tile in focus.
+   * @memberof MemoryGame
+   */
   _arrowUp (index) {
     const nextTile = index - this.cols
     if (nextTile >= 0) {
-      this._tileDiv.getElementsByTagName('a')[nextTile].focus()
+      this._gamediv.getElementsByTagName('a')[nextTile].focus()
+    } else {
+      return
     }
+    this._index = nextTile
   }
 
+  /**
+   * Method for when arrow left, or A is pressed on the keyboard.
+   *
+   * @param {number} index of the current tile in focus.
+   * @memberof MemoryGame
+   */
   _arrowLeft (index) {
     const nextTile = index - 1
     if (nextTile >= 0) {
-      this._tileDiv.getElementsByTagName('a')[nextTile].focus()
+      this._gamediv.getElementsByTagName('a')[nextTile].focus()
+    } else {
+      return
     }
+    this._index = nextTile
   }
 
+  /**
+   * Method for when arrow right, or D is pressed on the keyboard.
+   *
+   * @param {number} index of the current tile in focus.
+   * @memberof MemoryGame
+   */
   _arrowRight (index) {
     const nextTile = index + 1
     if (nextTile <= this._tileLimit) {
-      this._tileDiv.getElementsByTagName('a')[nextTile].focus()
+      this._gamediv.getElementsByTagName('a')[nextTile].focus()
+    } else {
+      return
     }
+    this._index = nextTile
   }
 
-  _turnTile (tile, index, img) {
-    let imgReference = null
-
-    if (img.nodeName === 'IMG') {
-      img.src = 'image/' + tile + '.png'
-      imgReference = img
-    } else {
-      img.firstElementChild.src = 'image/' + tile + '.png'
-      imgReference = img.firstElementChild
+  /**
+   * Method for turning a tile.
+   *
+   * @param {number} - Index of an array of tiles.
+   * @param {HTMLImageElement} - The current tile's reference to an image.
+   * @memberof MemoryGame
+   */
+  _turnTile (tile, img) {
+    if (this._turn2) {
+      return
     }
 
+    img.src = 'image/' + tile + '.png'
+
     if (!this._turn1) {
-      console.log('round1')
-      this._turn1 = imgReference
+      this._turn1 = img
       this._lastTile = tile
     } else {
-      console.log('round2')
-      this._turn2 = imgReference
-      if (tile === this._lastTile) {
-        this._turn1 = null
-        this._turn2 = null
-      } else {
-        setTimeout(() => {
-          console.log('reset')
-          this._turn1.src = 'image/0.png'
-          this._turn2.src = 'image/0.png'
+      if (img === this._turn1) {
+        return
+      }
 
-          this._turn1 = null
-          this._turn2 = null
-        }, 1000)
+      this._tries += 1
+      this.shadowRoot.querySelector('#tries').textContent = `Number of tries: ${this._tries}`
+
+      this._turn2 = img
+      if (tile === this._lastTile) {
+        this._pairs += 1
+        if (this._pairs === (this.cols * this.rows) / 2) {
+        }
+        this._pair(300)
+      } else {
+        this._noPair(1000)
       }
     }
   }
 
+  /**
+   * Method handling two tiles matching.
+   *
+   * @param {number} - Timelimit for the setTimeout.
+   * @memberof MemoryGame
+   */
+  _pair (time) {
+    setTimeout(() => {
+      this._turn1.style.visibility = 'hidden'
+      this._turn2.style.visibility = 'hidden'
+
+      this._turn1 = null
+      this._turn2 = null
+    }, time)
+  }
+
+  /**
+   * Method handling two tiles not matching.
+   *
+   * @param {number} - Timelimit for the setTimeout.
+   * @memberof MemoryGame
+   */
+  _noPair (time) {
+    setTimeout(() => {
+      this._turn1.src = 'image/0.png'
+      this._turn2.src = 'image/0.png'
+
+      this._turn1 = null
+      this._turn2 = null
+    }, time)
+  }
+
+  /**
+   * Creates and returns a shuffled array
+   * Based of rows * columns of the game with each number adding twice.
+   *
+   * @returns A shuffled array.
+   * @memberof MemoryGame
+   */
   _getPictureArray () {
     const arr = []
 
@@ -180,6 +233,13 @@ export class MemoryGame extends window.HTMLElement {
     return arr
   }
 
+  /**
+   * Takes an array of numbers and returns it shuffled.
+   *
+   * @param {[Array]} - An array of numbers.
+   * @returns A the passed array shuffled.
+   * @memberof MemoryGame
+   */
   fisherYatesShuffle (array) {
     let m = array.length
 
@@ -192,6 +252,63 @@ export class MemoryGame extends window.HTMLElement {
     }
 
     return array
+  }
+
+  connectedCallback () {
+    this._gamediv.getElementsByTagName('a')[0].focus()
+
+    this._gamediv.addEventListener('click', event => {
+      event.preventDefault()
+
+      let imgReference = null
+
+      if (event.target.nodeName === 'IMG') {
+        imgReference = event.target
+      } else {
+        return
+      }
+
+      this._index = parseInt(imgReference.getAttribute('data-tilenumber'))
+
+      this._turnTile(this._tiles[this._index], imgReference)
+      event.target.parentNode.focus()
+    })
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code - Inspiration
+    this._gamediv.addEventListener('keydown', (event) => {
+      if (event.defaultPrevented) {
+        return // Do nothing if event already handled
+      }
+
+      switch (event.code) {
+        case 'KeyS':
+        case 'ArrowDown':
+          this._arrowDown(this._index)
+          break
+        case 'KeyW':
+        case 'ArrowUp':
+          this._arrowUp(this._index)
+          break
+        case 'KeyA':
+        case 'ArrowLeft':
+          this._arrowLeft(this._index)
+          break
+        case 'KeyD':
+        case 'ArrowRight':
+          this._arrowRight(this._index)
+          break
+        case 'Enter' :
+          this._turnTile(this._tiles[this._index], event.target.firstElementChild)
+          break
+      }
+      // Consume the event so it doesn't get handled twice
+      event.preventDefault()
+    }, true)
+  }
+
+  disconnectedCallback () {
+    this._gamediv.addEventListener('click')
+    this._gamediv.addEventListener('keydown')
   }
 }
 
